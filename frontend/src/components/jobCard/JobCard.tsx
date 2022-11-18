99776bf2b6f129caf1831bb5d2bd7e96acc4fb94
Trungnhan01 <19211tt1749@mail.tdc.edu.vn>
2022-11-18 11:25:24 +0700

    updated related jobs

--- frontend/src/components/jobCard/JobCard.tsx
+++ frontend/src/components/jobCard/JobCard.tsx
@@ -16,6 +16,7 @@ const JobCard = ({ job }: Props) => {
     hoverable
     style={{ width: 240,borderRadius:'20px',marginBottom:'20px' }}
     cover={<div style={{background:`url('${job.company.imageUrl}') center no-repeat`,height:'200px'}}></div>}
+    bodyStyle={{borderTop:'1px solid #d3d3d3'}}
     onClick={() => navigate(`/job/${job.slug}`)}
   >
     <Card.Meta title={job?.jobTitle.title} style={{color:'#000000'}} description={getShortDescription(job.description, '.job-details__paragraph',40)} />
