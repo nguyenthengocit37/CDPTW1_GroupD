ecd80c506d66b2fe8a1417a3ed07b3fa7106703e
cuongp948 <cuongp948@gmail.com>
2022-12-01 12:49:23 +0700

    updated menu for header

--- frontend/src/layout/header/Header.tsx
+++ frontend/src/layout/header/Header.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import { Layout } from 'antd';
-import { HeaderWrapper } from './header.style';
+import { Menu, MenuItem, MenuButton, SubMenu } from '@szhsin/react-menu';
+import { HeaderWrapper, MenuWrapper } from './header.style';
 import { Link } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getSkills } from '../../services/api/skill/skillApi';
 const { Header: HeaderAntd } = Layout;
 function Header() {
+  const { data: skills } = useQuery(['skills'], () => {
+    return getSkills();
+  });
   return (
     <HeaderAntd>
       <HeaderWrapper>
         <Link to={'/'}>IT CAREER</Link>
+        <MenuWrapper>
+          <Menu menuButton={<MenuButton>All Jobs</MenuButton>} arrow>
+            <SubMenu label="Job By Skill" arrow align='start' overflow='auto'>
+              {skills &&
+                skills.length > 0 &&
+                skills.map((skill) => <MenuItem>{skill.name}</MenuItem>)}
+            </SubMenu>
+          </Menu>
+        </MenuWrapper>
       </HeaderWrapper>
     </HeaderAntd>
   );
--- frontend/src/layout/header/header.style.ts
+++ frontend/src/layout/header/header.style.ts
@@ -3,7 +3,22 @@ import styled from 'styled-components';
 export const HeaderWrapper = styled.div`
   color: #fff;
   font-size: 24px;
-  & > a {
+  display: flex;
+  & a {
     color: #fff;
   }
 `;
+export const MenuWrapper = styled.div`
+  font-size: initial;
+  & button{
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+  }
+  & button:hover {
+    color: #ea1e30;
+  }
+  & ul li{
+    line-height: initial;
+  }
+`
